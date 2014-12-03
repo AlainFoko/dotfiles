18da9a1aa7aa1d116ba1e9de73ad893b4eb379d7
Alain Achkar <aachkar@foko.co>
2014-12-02 20:22:21 -0500

    hidden files

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,4 @@
 alias reload!='. ~/.zshrc'
+
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO ; killall Finder /System/Library/CoreServices/Finder.app'
